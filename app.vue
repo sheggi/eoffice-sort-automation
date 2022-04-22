bea92602b2852c6f58f591bd6404afa3f2ab4a7e
Joel von Allmen <thesheggi@gmail.com>
2022-04-23 01:51:21 +0200

    add web ui

--- app.vue
+++ app.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="m-4">
+    <pre class=" whitespace-pre text-yellow-300">
+  ____                   _          _              _                                 _           
+ / ___|    ___    _ __  | |_       / \     _   _  | |_    ___    _ __ ___     __ _  | |_    __ _ 
+ \___ \   / _ \  | '__| | __|     / _ \   | | | | | __|  / _ \  | '_ ` _ \   / _` | | __|  / _` |
+  ___) | | (_) | | |    | |_     / ___ \  | |_| | | |_  | (_) | | | | | | | | (_| | | |_  | (_| |
+ |____/   \___/  |_|     \__|   /_/   \_\  \__,_|  \__|  \___/  |_| |_| |_|  \__,_|  \__|  \__,_|
+ </pre>
+
+    <div class="actionables">
+      <button @click="findActionables" class=" text-blue-500 hover:text-blue-400">refresh</button>
+      <div v-if="loading">loading ...</div>
+      <Actionable v-else v-for="actionable in actionables" :actionable="actionable" :key="actionable.id" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+const actionables = ref([])
+const loading = ref(true)
+
+const findActionables = async () => {
+  loading.value = true
+  const data = await (await fetch('/api/actionables')).json()
+
+  actionables.value = data.actionables
+  loading.value = false
+}
+
+onMounted(() => {
+  findActionables()
+})
+</script>
+
+<style >
+body {
+  background-color: #173347;
+  color: #cecece;
+  @apply font-mono;
+}
+</style>
---
+++ components/Actionable.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="actionable font-mono mt-4">
+        <div>{{ props.actionable.name }}</div>
+        <div>---</div>
+        <div>{{ props.actionable.description }}</div>
+        <div v-if="result" :class="{ ' text-green-600': status === 'success', 'text-red-800': status === 'error' }">&gt;
+            {{
+                    result
+            }}
+            <div v-if="status === 'error'">
+                <button @click="confirm" class="text-blue-500 hover:text-blue-400">retry</button>
+            </div>
+        </div>
+        <div v-else>?
+            <button @click="confirm" class="text-blue-500 hover:text-blue-400">yes</button>
+            or
+            <button @click="decline" class="text-blue-500 hover:text-blue-400">no</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['actionable'])
+const result = ref(null)
+const status = ref(null)
+
+const confirm = async () => {
+    try {
+        const response = await fetch('/api/action', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(props.actionable),
+        })
+        result.value = await response.json()
+        status.value = response.status === 200 ? 'success' : 'error'
+    } catch (err) {
+        result.value = err
+    }
+}
+
+const decline = () => {
+    console.log('wtf')
+    result.value = 'none'
+}
+</script>
+
