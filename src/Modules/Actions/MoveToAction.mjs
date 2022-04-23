3996d411290d3891699ce72e5187d5695db71d9f
Joel von Allmen <thesheggi@gmail.com>
2022-04-23 12:17:59 +0200

    use destination as string template

--- src/Modules/Actions/MoveToAction.mjs
+++ src/Modules/Actions/MoveToAction.mjs
@@ -5,23 +5,45 @@ import mv from 'mv'
 import Model from '../Model.mjs'
 
 export default class MoveToAction extends Model {
+  constructor() {
+    super()
+
+    this.destination = null
+
+    this.fileStat = null
+    this.source = null
+    this.target = null
+  }
+
   setDestination(destination) {
     this.destination = destination;
     return this;
   }
 
-  getDescription(fileStat) {
-    const source = fileStat.file;
-    const target = path.join(this.destination, fileStat.name);
-    return `move file from ${emphasize(source) } to ${emphasize(target)}`
+  prepare(fileStat) {
+    this.fileStat = fileStat
+
+    this.source = fileStat.file;
+
+    this.target = path.join(this.template(this.destination, this.fileStat));
+
+    return this
   }
 
-  async do(fileStat) {
-    const source = fileStat.file;
-    const target = path.join(this.destination, fileStat.name);
+  template(template, values) {
+    const replacer = (match, key) => values[key] || match
+    return template.replace(/\{\{([^{}]*)\}\}/g, replacer)
+  }
+
+  getDescription() {
+    return `move file from ${emphasize(this.source)} to ${emphasize(this.target)}`
+  }
+
+  async do() {
+    if (!this.source || !this.target) throw new Error('unprepared action')
 
     await new Promise((resolve, reject) => {
-      mv(source, target, { clobber: false }, (err) => err ? reject(err) : resolve())
+      mv(this.source, this.target, { clobber: false }, (err) => err ? reject(err) : resolve())
     })
 
     return true;
